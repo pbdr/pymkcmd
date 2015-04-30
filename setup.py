f503eeb684bf10f5ce8e8d155062e79ee2c3a6a9
dale <dale@pbdr.info>
2015-04-30 00:00:08 -0400

    `setup.py` and `MANIFEST.in` added. `.gitignore` updated.

---
+++ setup.py
@@ -0,0 +1,20 @@
+# pymkcmd | dale@pbdr.info | http://pbdr.info
+
+from setuptools import setup
+
+setup(
+
+    name='pymkcmd',
+
+    description='Make command line interfaces (CLI) for Python functions.',
+
+    url='https://github.com/pbdr/pymkcmd',
+
+    author='dale',
+    author_email='dale@pbdr.info',
+
+    license='MIT',
+
+    keywords='development',
+    packages=['pymkcmd']
+)
